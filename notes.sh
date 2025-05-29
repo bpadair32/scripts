e06b10f086dc22810d6c78fb851b4422fbe6c377
Brad Adair <bradadair@gmail.com>
2025-05-29 13:58:41 -0400

    Added notes script

---
+++ notes.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+directory=$HOME/Documents/Notes/
+
+newnote() {
+	name="$(echo "" | rofi -dmenu -p "Enter a name: " <&-)" || exit 0
+	: "${name:=$(date +%F_%T | tr ':' '-')}"
+	setsid -f "$TERMINAL" -e nvim $directory$name".md" >/dev/null 2>&1
+}
+
+selected_note() {
+	choice=$(echo -e "New\n$(command fd . $directory)" | rofi -dmenu - p "Choose note or create a new one")
+	case $choice in
+	New) newnote ;;
+	*.md)
+		setsid -f "$TERMINAL" -e nvim "$choice" >/dev/null 2>&1
+		;;
+	*) exit ;;
+	esac
+}
+
+selected_note
